05a19e9ff954ef6e5bd2457ddaeeb7855c3a05a9
nitianamit001-ctrl <nitianamit001@gmail.com>
2026-02-07 11:48:41 +0530

    complte 1 and 2 day in 2 day

---
+++ day1.cpp
@@ -0,0 +1,73 @@
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     cout<<"****\n";
+//     cout<<"***\n";
+//     cout<<"**\n";
+//     cout<<"*\n";
+
+//     return 0;
+// }
+
+//sum a & b            1
+
+// #include <iostream>
+//  using namespace std;
+//  int main(){
+//     int a;
+//     int b;
+//     cin>>a;
+//     cin>>b;
+//     cout<<a+b<<endl;
+//     return 0;
+// }
+
+//avg of marks              2
+
+// #include <iostream>
+//  using namespace std;
+//  int main(){
+//     int marks1;
+//     int marks2;
+//     int marks3;
+//     cout<<"enter marks1=";
+//     cin>>marks1;
+//     cout<<"enter marks2=";
+//     cin>>marks2;
+//     cout<<"enter marks3=";
+//     cin>>marks3;
+//     int avg=(marks1+marks2+marks3)/3;
+//     cout<<"avg marks="<<avg;
+//     return 0;
+//  }
+
+
+// #include <iostream>
+//  using namespace std;
+//  int main(){
+// cout<<bool(3)+2;                   1
+//  cout<<(23.5+2+'A');               2
+//   return 0;
+//  }
+
+
+//  day 2 pdf question
+
+
+#include <iostream>
+ using namespace std;
+ int main(){
+
+
+ return 0;
+ }
+
+
+
+
+
+
+
+
+
+
